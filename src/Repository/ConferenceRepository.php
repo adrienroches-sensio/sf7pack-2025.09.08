5166de0828bc9ed46a50163275172f0439343f93
Adrien Roches <adrien@roc-it.tech>
2025-09-10 10:39:21 +0200

    Add custom query on conference repository (between dates)

--- src/Repository/ConferenceRepository.php
+++ src/Repository/ConferenceRepository.php
@@ -3,8 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\Conference;
+use DateTimeImmutable;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use InvalidArgumentException;
 
 /**
  * @extends ServiceEntityRepository<Conference>
@@ -14,6 +16,34 @@ class ConferenceRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Conference::class);
+    }
+
+    /**
+     * @return list<Conference>
+     */
+    public function searchBetweenDates(DateTimeImmutable|null $start = null, DateTimeImmutable|null $end = null): array
+    {
+        if (null === $start && null === $end) {
+            throw new InvalidArgumentException('At least one date is required to operate this method.');
+        }
+
+        $qb = $this->createQueryBuilder('conference');
+
+        if (null !== $start) {
+            $qb
+                ->andWhere('conference.startAt >= :start')
+                ->setParameter('start', $start)
+            ;
+        }
+
+        if (null !== $end) {
+            $qb
+                ->andWhere('conference.endAt <= :end')
+                ->setParameter('end', $end)
+            ;
+        }
+
+        return $qb->getQuery()->getResult();
     }
 
     //    /**
