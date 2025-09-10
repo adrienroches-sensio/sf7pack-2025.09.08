24459f49c3ac142985582c45ef9536e4d6fd0a96
Adrien Roches <adrien@roc-it.tech>
2025-09-10 14:05:00 +0200

    Fill template + modify new controller for conferences to use the newly created form

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -5,42 +5,32 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Conference;
+use App\Form\ConferenceType;
 use App\Repository\ConferenceRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
-use function array_map;
 
 class ConferenceController extends AbstractController
 {
     #[Route(
-        path: '/conference/{name}/{start}/{end}',
+        path: '/conference/new',
         name: 'app_conference_new',
-        requirements: [
-            'start' => Requirement::DATE_YMD,
-            'end' => Requirement::DATE_YMD,
-        ],
         methods: ['GET'],
     )]
-    public function newConference(string $name, string $start, string $end, EntityManagerInterface $em): Response
+    public function newConference(EntityManagerInterface $em): Response
     {
-        $conference = (new Conference())
-            ->setName($name)
-            ->setDescription('Some generic description')
-            ->setAccessible(true)
-            ->setStartAt(new \DateTimeImmutable($start))
-            ->setEndAt(new \DateTimeImmutable($end))
-        ;
+        $conference = new Conference();
 
-        $em->persist($conference);
-        $em->flush();
+        $form = $this->createForm(ConferenceType::class, $conference);
 
-        return new Response("Conference nº{$conference->getId()} created");
+        return $this->render('conference/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
     #[Route(
