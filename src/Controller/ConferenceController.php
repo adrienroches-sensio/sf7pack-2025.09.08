f678335e0a3db93a58ea61921cd1b42bb1913fe3
Adrien Roches <adrien@roc-it.tech>
2025-09-12 11:45:44 +0200

    auto set created_by on new conference

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -33,6 +33,8 @@ class ConferenceController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $conference->setCreatedBy($this->getUser());
+
             $em->persist($conference);
             $em->flush();
 
